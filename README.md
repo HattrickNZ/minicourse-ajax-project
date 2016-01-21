22eda801dc70d99c5002eaee9afd6e459c330afa
HattrickNZ <youngkevinsmith@hotmail.com>
2016-01-22 11:19:12 +1300

    my version with my slight changes/learnings

---
+++ README.md
@@ -0,0 +1,20 @@
+Front-End Web Developer Nanodegree
+https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001
+PROJECT Neighborhood Map
+
+You will develop a single-page application featuring a map of your neighborhood or a neighborhood you would like to visit. You will then add additional functionality to this application, including: map markers to identify 
+popular locations or places you’d like to visit, a search function to easily discover these locations, and a listview to support simple browsing of all locations. You will then research and implement third-party APIs that 
+provide additional information about each of these locations (such as StreetView images, Wikipedia articles, Yelp reviews, etc).
+
+Prepare for this project with: Intro to AJAX and JavaScript Design Patterns
+https://www.udacity.com/course/intro-to-ajax--ud110
+https://www.udacity.com/course/javascript-design-patterns--ud989
+
+## this is where I am in the intro to Ajax -- finished now
+https://www.udacity.com/course/viewer#!/c-ud110/l-3310298553/m-3191498539
+
+
+TODO: 
+1 change the new your times sourse to other news papers sources 
+2 add other sources
+
--- js/script.js
+++ js/script.js
@@ -25,15 +25,43 @@ function loadData() {
            address + '">';
 
     $body.append(streetviewUrl);
-
-    $.getJSON("http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+$city+"&sort=newest&api-key=<insert key here>",
+	
+	// me working througth the solution
+	var nyTimesURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+$city+"&sort=newest&api-key=a5486bd5b73442ce353f3931ddc278b1:2:74079982";
+	console.log("nyTimesURL",nyTimesURL);
+	$.getJSON(nyTimesURL, function (data) {
+		console.log("nytimesAPI",data);
+		
+		var docs = data.response.docs;
+		//console.log("data.response.docs",data.response.docs);
+        var articles = [];
+		//console.log("articles",articles);
+		
+		$.each(docs, function(index, value){
+			//console.log("index",index);
+			//console.log("value.document_type",value.document_type);
+			if(value.document_type == 'article') {
+				articles.push('<li class="article"><a href="'+value.web_url+'">'+value.headline.main+'</a><p>' + value.snippet + '</p>');
+			}
+		});
+		//console.log("articles",articles);
+		//console.log("articles[0]",articles[0]);
+		
+		$(articles.join( " " )).appendTo( $("#nytimes-articles") );
+		//console.log("articles.join( \" \" )",articles.join( " " ));
+	});
+	
+	
+	/*
+    $.getJSON("http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+$city+"&sort=newest&api-key=a5486bd5b73442ce353f3931ddc278b1:2:74079982",
         function( data ) {
+			console.log("nytimesAPI",data);
             var docs = data.response.docs;
             var articles = [];
 
             $.each(docs, function(index, value){
                 if(value.document_type == 'article') {
-                    articles.push('<li class="article"><a href="'+value.web_url+'">'+value.headline.main+'</a><p>' + value.snippet + '</p>');
+                    articles.push('<li class="article"><a href="'+value.web_url+'">'+value.headline.main +' -- ' + value.source +'</a><p>' + value.snippet + '</p>');
                 }
             });
 
@@ -42,15 +70,45 @@ function loadData() {
             // udacity solution
             // for(...)
             //   $nytElem.append(<article list item>);
-        }).error(function() {
+        }).error(function(e) {
             $nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
+			console.log("e",e)
         }
     );
-
+		*/
+		
+		
+	// this gets the timer going and if is not cleared after 8 seconds $wikiElem value will be changed 
     var wikiRequestTimeout = setTimeout(function(){
         $wikiElem.text("failed to get wikipedia resources");
+		console.log("date",Date());
     }, 8000);
+	
 
+	
+	//my version working through it 
+	var wikiURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+$city+'&format=json&callback=wikiCallBack';
+	console.log("wikiURL",wikiURL);
+	$.ajax(wikiURL, {
+			//url : wikiURL, // this would work also
+			dataType: "jsonp",
+			error: function(e){consiole.log("e",e);},
+			success: function(response) {
+				console.log("wikiResponse",response);
+				var articleTitles = response[1];
+				var articleLinks = response[3];
+				var numberOfArticles = articleLinks.length;
+				for(var i = 0; i < numberOfArticles; i++) {
+                $wikiElem.append('<li><a href="'+articleLinks[i]+'">'+articleTitles[i]+'</a></li>');
+				}
+				console.log("date",Date());
+				
+				clearTimeout(wikiRequestTimeout);
+
+			}
+	});
+	
+	/*
     $.ajax('http://en.wikipedia.org/w/api.php?action=opensearch&search='+$city+'&format=json&callback=wikiCallBack', {
         dataType: "jsonp",
         success: function(response) {
@@ -64,13 +122,19 @@ function loadData() {
 
             clearTimeout(wikiRequestTimeout);
         }
-    });
+    });*/
 
     return false;
 }
 
 $('#form-container').submit(loadData);
 
+
+//$('#form-container').submit(loadData2);
+
+function loadData2() {
+	alert("this is loadData2...this function is running now...")
+}
 
 
 // loadData();
